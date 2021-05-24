0b90e47e6022cf45317e74d931a7d85a4c849496
TKZET <tahaktobias@gmail.com>
2021-05-24 15:59:24 -0300

    Modificando os field em observaçãoforms

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 DEFAULT_APPS = [
-    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
